1fe1d9d8cb16b06db0530f48628c66c33f43a2e6
boinged <stevenshipton@hotmail.com>
2022-10-05 12:51:28 +0000

    add config port

---
+++ config/config.go
@@ -0,0 +1,12 @@
+package config
+
+import "os"
+
+var Port string
+
+func init() {
+	Port = os.Getenv("PORT")
+	if Port == "" {
+		Port = "8080"
+	}
+}
--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/boinged/api-app-go/config"
 )
 
 func main() {
 	http.HandleFunc("/", func(response http.ResponseWriter, request *http.Request) {
 		fmt.Fprint(response, "Welcome")
 	})
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":" + config.Port, nil)
 }
